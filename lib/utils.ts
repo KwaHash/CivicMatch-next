ed08603c28a8ecd88748337a30c0f579b77c1ba7
KwaHash <cosmosray001@gmail.com>
2025-12-26 11:14:31 +0900

    feat: politician register page

--- lib/utils.ts
+++ lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function toLocaleDateString(date: string | Date | undefined) {
+  if (!date) return ''
+  return new Date(date).toLocaleDateString('ja-JP', {
+    year: 'numeric',
+    month: '2-digit',
+    day: '2-digit',
+  }).replaceAll('/', '.')
+}
